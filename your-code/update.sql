95dc31b4b5f612441f5889e7fe5b3cbe8f779e32
CarolM <202971594+CarolDyh@users.noreply.github.com>
2025-07-14 16:30:44 +0200

    Ejercicios resueltos

---
+++ your-code/update.sql
@@ -0,0 +1,15 @@
+UPDATE Vendedores
+SET tienda = 'Miami'
+WHERE nombre = 'Paige Turner' AND tienda = 'Mimia';
+
+UPDATE Clientes
+SET email = 'ppicasso@gmail.com'
+WHERE nombre = 'Pablo Picasso';
+
+UPDATE Clientes
+SET email = 'lincoln@us.gov'
+WHERE nombre = 'Abraham Lincoln';
+
+UPDATE Clientes
+SET email = 'hello@napoleon.me'
+WHERE nombre = 'Napoléon Bonaparte';
